9c4f2d955a37c5e6da7641080e53811c3e0ba802
Niklas Mack <nigi.m4.a1@gmail.com>
2018-01-26 11:53:33 +0100

    Fix directory to clone into

--- clone.sh
+++ clone.sh
@@ -7,6 +7,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
   repo=${line##*/}
   echo "---------------------------------- CLONE REPO"
   git clone "git@github.com:$line.git $line"
+  git clone "git@github.com:$line.git" $line
   cd $line
 
   echo "---------------------------------- FETCH"
